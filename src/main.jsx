938f6919ba97a99db578308cc92a1aa39951bba9
SofNyb <snjwindeloev@gmail.com>
2025-11-28 12:52:24 +0100

    routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,39 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+
+        <Route path="login" />
+        <Route path="register" />
+
+        <Route path="profile/:id">
+          <Route path="bookmark" />
+          <Route path="rating" />
+          <Route path="searchhistory" />
+        </Route>
+
+        <Route path="title" />
+        <Route path="title/:tconst">
+          <Route path="cast" />
+          <Route path="genre" />
+          <Route path="episode" />
+          <Route path="award" />
+          <Route path="overallrating" />
+          <Route path="principals" />
+        </Route>
+
+        <Route path="name" />
+        <Route path="name/:nconst">
+          <Route path="knownfor" />
+          <Route path="profession" />
+          <Route path="role" />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
