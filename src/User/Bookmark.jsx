4c33aee1d15fe185f17e574c44c108260c3793a7
SofNyb <snjwindeloev@gmail.com>
2025-12-01 00:41:43 +0100

    Bookmark component

--- src/User/Bookmark.jsx
+++ src/User/Bookmark.jsx
@@ -1,13 +1,34 @@
-import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
 import { FaChevronRight } from "react-icons/fa";
+import { Card } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import { bookmarks, movies } from "../data/dummyData";
 
 const Bookmark = () => {
   return (
-    <>
-      Bookmarks
-      <FaChevronRight />
-    </>
+    <div>
+      <p>
+        Bookmark
+        <FaChevronRight />
+      </p>
+
+      <Row className="g-3">
+        {bookmarks.map((bookmark) => {
+          const movie = movies.find((m) => m.id === bookmark.movieId);
+          return (
+            <Col key={bookmark.id} md={6}>
+              <Card>
+                <Card.Body>
+                  <Card.Text>{movie?.title}</Card.Text>
+                  <Card.Text className="text-muted small">
+                    {bookmark.addedDate}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </div>
   );
 };
 
