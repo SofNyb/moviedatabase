32e21fdb61bffe0f0ebbbd07d8cd27d54792d936
SofNyb <snjwindeloev@gmail.com>
2025-12-01 00:39:48 +0100

    rmv container import

--- src/User/UserHero.jsx
+++ src/User/UserHero.jsx
@@ -1,6 +1,5 @@
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { FaCalendar, FaCog, FaShareAlt, FaUserCircle } from "react-icons/fa";
