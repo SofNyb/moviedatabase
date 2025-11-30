1aa53eb94f7f951ee1152d7aec3d345f63d11dbc
SofNyb <snjwindeloev@gmail.com>
2025-11-30 23:58:20 +0100

    hero updated

--- src/User/UserHero.jsx
+++ src/User/UserHero.jsx
@@ -1,10 +1,57 @@
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { FaCalendar, FaCog, FaShareAlt, FaUserCircle } from "react-icons/fa";
 
 function UserHero() {
   return (
-    <Card>
-      <Card.Body>This is some text within a card body.</Card.Body>
-    </Card>
+    <Container className="my-4 p-4 border rounded">
+      <Row>
+        <Col md={6}>
+          <div className="d-flex flex-column align-items-start">
+            <div className="mb-3">
+              <FaUserCircle size={80} />
+            </div>
+            <Card.Title>User name</Card.Title>
+
+            <Card.Text>
+              <FaCalendar /> Joined at DATE
+            </Card.Text>
+            <Button variant="dark">Edit Profile</Button>
+          </div>
+        </Col>
+
+        <Col md={6}>
+          <Row className="mb-2">
+            <Col xs={11} className="d-flex justify-content-end">
+              <FaShareAlt size={20} />
+              <FaCog size={20} />
+            </Col>
+          </Row>
+
+          <Row className="g-3">
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Bookmarks</Card.Title>
+                  <Card.Text>Antal</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Ratings</Card.Title>
+                  <Card.Text>Antal</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
