56868e453820dbd73e4b501be567dc1ffb2718fb
SofNyb <snjwindeloev@gmail.com>
2025-12-01 00:39:39 +0100

    rating component on userpage

--- src/User/Rating.jsx
+++ src/User/Rating.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { Card } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import { FaChevronRight } from "react-icons/fa";
+import { ratings, movies } from "../data/dummyData";
 
 const Rating = () => {
-  return <div>Rating</div>;
+  return (
+    <div>
+      <p>
+        Ratings
+        <FaChevronRight />
+      </p>
+
+      <Row className="g-3">
+        {ratings.map((rating) => {
+          const movie = movies.find((m) => m.id === rating.movieId);
+
+          return (
+            <Col key={rating.id} md={6}>
+              <Card>
+                <Card.Body>
+                  <Card.Text>{movie?.title}</Card.Text>
+                  <Card.Text>Rating: {rating.rating}/10</Card.Text>
+                  <Card.Text>{rating.review}</Card.Text>
+                  <Card.Text className="text-muted small">
+                    {rating.ratedDate}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </div>
+  );
 };
 
 export default Rating;
