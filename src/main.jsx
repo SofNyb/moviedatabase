7f7d1b3927b848e4fc98363988a337808617bc77
SofNyb <snjwindeloev@gmail.com>
2025-12-01 01:06:57 +0100

    add  the new navbar name and correctly place inside the router

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar.jsx";
 import UserPage from "./User/userPage.jsx";
+import Navigation from "./Components/Navbar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Navbar />
     <BrowserRouter>
+      <Navigation />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="login" />
