607bbb5513cc85dad0f81e8b2271817e05662dbb
SofNyb <snjwindeloev@gmail.com>
2025-12-09 11:42:37 +0100

    connection to the searchhistory-table

--- src/Components/Nav/Search.jsx
+++ src/Components/Nav/Search.jsx
@@ -2,6 +2,7 @@ import { BsSearch } from "react-icons/bs";
 import { useEffect, useState } from "react";
 import { Dropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { userService } from "../../services/userService.js";
 
 // search as you type / debounced saving¨
 // debounce -> waiting for the user to stop typing for x amount of time before doing smt
@@ -15,11 +16,24 @@ const Search = () => {
   const [showSearch, setShowSearch] = useState(false);
 
   // recent searches
+  // useEffect(() => {
+  //   const storedSearches = JSON.parse(localStorage.getItem("recentSearches"));
+  //   if (storedSearches) {
+  //     setRecentSearches(storedSearches);
+  //   }
+  // }, []);
+
   useEffect(() => {
-    const storedSearches = JSON.parse(localStorage.getItem("recentSearches"));
-    if (storedSearches) {
-      setRecentSearches(storedSearches);
-    }
+    const fetchSearchHistory = async () => {
+      try {
+        const searchHistory = await userService.getSearchHistory();
+        setRecentSearches(searchHistory);
+      } catch (error) {
+        console.error("Error fetching recent searches:", error);
+      }
+    };
+
+    fetchSearchHistory();
   }, []);
 
   //debounce
@@ -27,14 +41,19 @@ const Search = () => {
     let timer;
     //timer for 1.5 sec.
     if (search.length > 3) {
-      timer = setTimeout(() => {
-        setRecentSearches((prevSearches) => {
-          // if theres duplicate searches, dont save
-          const filtered = prevSearches.filter((item) => item !== search);
-          const updated = [search, ...filtered].slice(0, 5);
-          localStorage.setItem("recentSearches", JSON.stringify(updated));
-          return updated;
-        });
+      timer = setTimeout(async () => {
+        try {
+          await userService.saveSearch(search);
+          // Then update local state
+          setRecentSearches((prev) => {
+            const filtered = prev.filter(
+              (item) => item.search_string !== search
+            );
+            return [search, ...filtered].slice(0, 5);
+          });
+        } catch (error) {
+          console.error("Failed to save search:", error);
+        }
       }, 1500);
     }
     // clears timer if user types again before timer ends
@@ -61,15 +80,19 @@ const Search = () => {
         <div className="position-absolute">
           <Dropdown.Menu show className="w-100">
             {recentSearches.map((item, index) => (
-              <Dropdown.Item as={Link} to={`/search/${item}`} key={index}>
-                {item}
+              <Dropdown.Item
+                as={Link}
+                to={`/search/${item.search_string}`}
+                key={index}
+              >
+                {item.search_string}
               </Dropdown.Item>
             ))}
             <Dropdown.Divider />
             <Dropdown.Item
               className="text-center"
               as={Link}
-              to="/search/history"
+              to="/searchhistory"
             >
               All recent searches
             </Dropdown.Item>
