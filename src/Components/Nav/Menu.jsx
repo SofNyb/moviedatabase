b29d474d78c29c15e75f45980d986361ae14df4b
SofNyb <snjwindeloev@gmail.com>
2025-12-15 15:01:46 +0100

    removed unused code

--- src/Components/Nav/Menu.jsx
+++ src/Components/Nav/Menu.jsx
@@ -5,8 +5,6 @@ const Menu = () => {
     <NavDropdown title="Menu" id="basic-nav-dropdown">
       <NavDropdown.Item href="/titles">All titles</NavDropdown.Item>
       <NavDropdown.Item href="/names">All actors</NavDropdown.Item>
-    {/* <NavDropdown.Divider />
-      <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
     </NavDropdown>
   );
 };
